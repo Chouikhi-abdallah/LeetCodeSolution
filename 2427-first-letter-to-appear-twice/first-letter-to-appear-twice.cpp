d33dfd68798f789f3495ba0aed7ebde098858e7f
Chouikhi Abdallah <136018238+Chouikhi-abdallah@users.noreply.github.com>
2024-01-27 16:04:41 +0100

    Time: 3 ms (36.14%) | Memory: 7.5 MB (8.59%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        map<char,int> mp;
+        char ch;
+        for (auto i:s){
+            mp[i]++;
+            if(mp[i]==2){
+                 ch=i;
+                 break;
+        }} 
+    return ch;
+    }
+};
