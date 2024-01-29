d7535e386d0cae1688c1c0dfb3ed92717c44de0a
Chouikhi Abdallah <136018238+Chouikhi-abdallah@users.noreply.github.com>
2024-01-29 13:18:37 +0100

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.04%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,36 @@
+class MyQueue {
+private:
+   stack<int> st1;
+   stack<int>st2;
+
+public:
+    MyQueue() {}
+
+    void push(int x) {
+        while(!st1.empty()){
+            
+            st2.push(st1.top());
+            st1.pop();
+        }
+        st1.push(x);
+        while(!st2.empty()){
+            st1.push(st2.top());
+            st2.pop();
+        }
+        
+    }
+
+    int pop() {
+        int x=st1.top();
+        st1.pop();
+       return x;
+    }
+
+    int peek() {
+        return st1.top();
+    }
+
+    bool empty() {
+        return st1.empty();
+    }
+};
