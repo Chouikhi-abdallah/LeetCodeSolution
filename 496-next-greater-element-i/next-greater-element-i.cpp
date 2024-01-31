92d8e5655cc1def28498423c087f87f0a01fe9e0
Chouikhi Abdallah <136018238+Chouikhi-abdallah@users.noreply.github.com>
2024-01-31 15:16:31 +0100

    Time: 17 ms (16.74%) | Memory: 11.3 MB (18.43%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> mp;
+    for (auto &i:nums1) {
+        for (int j=0;j<nums2.size();j++) {
+            if(i==nums2[j])
+                mp[i]=j;
+        }
+    }
+    vector<int>v;
+    for (auto i :nums1) {
+        for (int j = mp[i]; j <nums2.size() ; j++) {
+            if(nums2[j]>i) {
+                v.push_back(nums2[j]);
+                break;
+            }
+            if(j==nums2.size()-1)
+            v.push_back(-1);
+
+        }
+
+    }
+    return v;
+    }
+};
