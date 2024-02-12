beb45990e10bec3b4ed42801742850dc3781f346
Chouikhi Abdallah <136018238+Chouikhi-abdallah@users.noreply.github.com>
2024-02-12 13:37:42 +0100

    Time: 17 ms (35.13%) | Memory: 22.2 MB (18.20%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=int (nums.size());
+        map<int,int> mp;
+        int x;
+        for(auto &i:nums){
+            mp[i]++;
+            if (mp[i]>n/2)
+                x= i;    
+        }
+        return x;
+    }
+};
