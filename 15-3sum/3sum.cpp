d5e7e160772339ff6fc3ba5ac3f672e2ce4a1ac0
Chouikhi Abdallah <136018238+Chouikhi-abdallah@users.noreply.github.com>
2024-02-21 17:43:35 +0100

    Time: 2137 ms (5.01%) | Memory: 504.6 MB (5.09%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int leftPointer=0;
+    std::sort(nums.begin(), nums.end());
+    vector<vector<int>> vvi;
+    set<vector<int>>svi;
+
+    while(leftPointer<=int(nums.size()-3)){
+        int left=leftPointer+1;
+        int right=int (nums.size()-1);
+        while(left<right){
+            if(nums[leftPointer]+nums[right]+nums[left]==0){
+                svi.insert({nums[leftPointer],nums[right],nums[left]});
+                left++;
+            }
+            else if(nums[leftPointer]+nums[right]+nums[left]<0){
+                left++;
+            }
+            else{
+                right--;
+            }
+
+        }
+        leftPointer++;
+    }
+    for (auto i:svi) {
+        vvi.push_back(i);
+
+    }
+    return vvi;
+    }
+};
