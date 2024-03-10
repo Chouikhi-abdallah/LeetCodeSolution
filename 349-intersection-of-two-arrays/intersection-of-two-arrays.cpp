ad705810cf2e99d9a5f25f9a71de0ff72f355115
Chouikhi Abdallah <136018238+Chouikhi-abdallah@users.noreply.github.com>
2024-03-10 18:32:38 +0100

    Time: 14 ms (9.25%) | Memory: 13.6 MB (12.68%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -20,14 +20,14 @@ vector<int> theMinimumSize(vector<int> &v1,vector<int> &v2){
     set<int>s1;
     set<int>s2;
     vector<int>res;
-    for (auto i:v2) {
+    for (auto &i:v2) {
         s1.insert(i);
     }
-    for (auto i:v1) {
+    for (auto &i:v1) {
         s2.insert(i);
     }
-    for (auto i:s1) {
-        for (auto j:s2) {
+    for (auto &i:s1) {
+        for (auto &j:s2) {
             if(i==j)
                 res.push_back(i);
         }
