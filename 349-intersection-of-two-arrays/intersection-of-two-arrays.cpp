3ae81cbd1470ebb635a4e724b9a8b5b132f33001
Chouikhi Abdallah <136018238+Chouikhi-abdallah@users.noreply.github.com>
2024-03-10 18:31:06 +0100

    Time: 7 ms (41.63%) | Memory: 13.4 MB (15.40%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+vector<int> theBiggestSize(vector<int> &v1,vector<int> &v2){
+   if(v1.size()>v2.size())
+       return v1;
+   else
+       return v2;
+
+}
+vector<int> theMinimumSize(vector<int> &v1,vector<int> &v2){
+    if(v1.size()<v2.size())
+        return v1;
+    else
+        return v2;
+
+}
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> v1= theBiggestSize(nums1,nums2);
+    vector<int> v2= theMinimumSize(nums1,nums2);
+    set<int>s1;
+    set<int>s2;
+    vector<int>res;
+    for (auto i:v2) {
+        s1.insert(i);
+    }
+    for (auto i:v1) {
+        s2.insert(i);
+    }
+    for (auto i:s1) {
+        for (auto j:s2) {
+            if(i==j)
+                res.push_back(i);
+        }
+
+    }
+    return res;
+    }
+};
