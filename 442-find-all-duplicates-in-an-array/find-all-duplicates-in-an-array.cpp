88adebf1c1fa919deff85f742a770d0ada8274f0
Chouikhi Abdallah <136018238+Chouikhi-abdallah@users.noreply.github.com>
2024-03-25 01:11:00 +0100

    Time: 135 ms (5.17%) | Memory: 48.9 MB (12.33%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int>v;
         map<int,int>mp;
-        for(auto i:nums){
+        for(auto &i:nums){
             mp[i]++;
             if(mp[i]==2)
                 v.push_back(i);
