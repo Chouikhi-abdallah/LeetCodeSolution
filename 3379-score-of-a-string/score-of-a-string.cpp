bf13fc5128fa90331bb3260e645ab3b668f9d057
Chouikhi Abdallah <chouikhiabdallahpro@gmail.com>
2024-06-07 19:27:47 +0100

    Time: 0 ms (100.00%) | Memory: 7.8 MB (41.41%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum=0;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            sum+=abs(s[i]-s[i+1]);
+        }
+        return sum;
+        
+    }
+};
