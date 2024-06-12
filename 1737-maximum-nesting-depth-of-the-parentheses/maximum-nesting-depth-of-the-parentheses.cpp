836bdd4cb5e9cbb23cbb8a145b04feb79bbe42b8
Chouikhi Abdallah <chouikhiabdallahpro@gmail.com>
2024-06-12 15:37:34 +0100

    Time: 0 ms (100.00%) | Memory: 7.4 MB (50.76%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+            stack<char> track;
+    int maxDepth=0;
+    for (char i : s) {
+        if(i=='(')
+            track.push(i);
+        if(i==')'){
+            if(maxDepth<=track.size())
+                maxDepth=int(track.size());
+            track.pop();
+        }
+
+
+    }
+    return maxDepth;
+
+    }
+};
