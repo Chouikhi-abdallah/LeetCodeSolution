f1d16d769bce5359b2682cd7336926f359732e4d
Chouikhi Abdallah <chouikhiabdallahpro@gmail.com>
2024-07-05 12:14:56 +0100

    Time: 0 ms (100.00%) | Memory: 7.3 MB (19.85%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+      string res;
+
+      for(int i=0;i<address.length();i++){
+        if(address[i]!='.')
+            res+=address[i];
+        else{
+            res+="[.]";
+        }
+
+      }
+      return res;
+        
+    }
+};
