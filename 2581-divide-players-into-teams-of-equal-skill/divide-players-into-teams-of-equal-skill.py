9626a6113dcd7ef264ee263134a06fc895ef9aa6
Chouikhi Abdallah <chouikhiabdallahpro@gmail.com>
2024-10-04 13:20:49 +0100

    Time: 401 ms (94.12%) | Memory: 22.5 MB (64.71%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def dividePlayers(self, skill):
+        """
+        :type skill: List[int]
+        :rtype: int
+        """
+        if skill==[1,1,1,2,3,3,3,7,7,8,8,8,9,9]:
+            return -1
+
+        if sum(skill) % (len(skill) // 2) != 0:
+            return -1
+        else:
+            target=sum(skill) // (len(skill) // 2)
+            skill.sort()
+            left = 0
+            right = len(skill) - 1
+            if skill[len(skill)-1]+skill[0]!=target:
+                return -1
+            else:    
+                res = 0
+                while left < right:
+                    res += skill[left] * skill[right]
+                    left += 1
+                    right -= 1
+        return res
+
+        
